7f916172276bde5630b5230a4785171beb96f5e2
Timothee Guerin <tiguerin@microsoft.com>
2026-04-15 11:28:17 -0400

    use msbuild

--- packages/http-server-csharp/src/components/project/csproj.tsx
+++ packages/http-server-csharp/src/components/project/csproj.tsx
@@ -1,5 +1,13 @@
 import { type Children } from "@alloy-js/core";
 import { CsprojFile } from "@alloy-js/csharp";
+import {
+  ImplicitUsings,
+  ItemGroup,
+  Nullable,
+  PackageReference,
+  PropertyGroup,
+  TargetFramework,
+} from "@alloy-js/msbuild/components";
 
 export interface CsprojProps {
   /** The project name (without extension). */
@@ -12,17 +20,18 @@ export interface CsprojProps {
  * Renders a .csproj file for the ASP.NET service project.
  */
 export function Csproj(props: CsprojProps): Children {
-  const swaggerPackage = props.useSwaggerUI
-    ? `\n  <ItemGroup>\n    <PackageReference Include="SwashBuckle.AspNetCore" Version="7.3.1" />\n  </ItemGroup>`
-    : "";
-
   return (
     <CsprojFile path={`${props.projectName}.csproj`} sdk="Microsoft.NET.Sdk.Web">
-      {`  <PropertyGroup>
-    <TargetFramework>net9.0</TargetFramework>
-    <Nullable>enable</Nullable>
-    <ImplicitUsings>enable</ImplicitUsings>
-  </PropertyGroup>${swaggerPackage}`}
+      <PropertyGroup>
+        <TargetFramework>net9.0</TargetFramework>
+        <Nullable>enable</Nullable>
+        <ImplicitUsings>enable</ImplicitUsings>
+      </PropertyGroup>
+      {props.useSwaggerUI && (
+        <ItemGroup>
+          <PackageReference Include="SwashBuckle.AspNetCore" Version="7.3.1" />
+        </ItemGroup>
+      )}
     </CsprojFile>
   );
 }
